665a768aed790896dfddf2b123e21aa2de0f8b7a
jjlee5266 <jjlee5266@gmail.com>
2015-01-20 16:50:45 -0500

    inventory controller start

--- app/assets/javascripts/backbone/views/ModelView.js
+++ app/assets/javascripts/backbone/views/ModelView.js
@@ -1,13 +1,20 @@
 App.Views.LocationView = Backbone.View.extend({
   tagName: 'div',
   className: 'location',
+  events:{
+    'click .showInventory': 'renderInventory'
+  },
   initialize:function(){
     console.log("One VIew Created");
     this.locationTemplate = HandlebarsTemplates['location'];
     this.render();
+    this.listenTo(this.model,"show",this.renderInventory)
   },
   render: function(){
     this.$el.html(this.locationTemplate(this.model.toJSON()));
+  },
+  renderInventory:function(){
+    console.log("moo");
   }
 
 });
