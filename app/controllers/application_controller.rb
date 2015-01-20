066addad216a23435c2700c6385dfafb87e97124
jjlee5266 <jjlee5266@gmail.com>
2015-01-20 15:35:05 -0500

    playing with devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  helper_method :current_user
 
   def index
     render layout: "application",text: ""
   end
+
+  
 end
