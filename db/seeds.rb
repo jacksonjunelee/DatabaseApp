a1b35062b591cbc0e58112688a90a602a0685127
jjlee5266 <jjlee5266@gmail.com>
2015-01-20 12:04:15 -0500

    added devise

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Location.create(name: "MundiOro", address: "540 sw 56st", city: "New York", state: "NY", zip: "92929")
+Location.create(name: "CatOro", address: "522 sw 55st", city: "New York", state: "NY", zip: "11111")
+Location.create(name: "MewOro", address: "567 sw 56st", city: "New York", state: "NY", zip: "22222")
+
+# id | name | address | city | state | zip | headquarters_id | created_at | updated_at
