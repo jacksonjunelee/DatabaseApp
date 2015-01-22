c9d3f6791ee691deabb23abc8eb1665098809107
jjlee5266 <jjlee5266@gmail.com>
2015-01-22 15:47:12 -0500

    add branch feature

--- app/assets/javascripts/backbone/views/locationList.js
+++ app/assets/javascripts/backbone/views/locationList.js
@@ -11,6 +11,7 @@ App.Views.LocationListView = Backbone.View.extend({
   },
   renderLocation:function(location){
     var locationView = new App.Views.LocationView({model: location});
+    locationView.render();
     this.$el.append(locationView.$el);
   }
 
